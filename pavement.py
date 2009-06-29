3d9041d9e342ee7ceed9a72f8c90e10863455802
soswow <soswow@gmail.com>
2009-06-29 17:39:22 +0000

    Some setup issues.

--- pavement.py
+++ pavement.py
@@ -1,21 +1,22 @@
 from paver.setuputils import setup
+from paver.setuputils import find_package_data
 from paver.easy import *
 
 setup(
-    name="pypsd",
-    packages=['pypsd'],
-    package_data={'pypsd': ['pypsd/conf/*.conf', 'samples/*.psd', 'docs/*.pdf']},
-    version="0.1",
-    url="http://code.google.com/p/pypsd",
-    author="Aleksandr Motsjonov",
-    author_email="soswow@gmail.com",
-    sphinx=Bunch(
-        builddir="_build"
-    )
+	name="pypsd",
+	packages=['pypsd'],
+	package_data=find_package_data('pypsd', 'pypsd', only_in_packages=False),
+	version="0.1",
+	url="http://code.google.com/p/pypsd",
+	author="Aleksandr Motsjonov",
+	author_email="soswow@gmail.com",
+	sphinx=Bunch(
+		builddir="_build"
+	)
 )
 
 @task
 @needs('setuptools.command.sdist')
 def sdist():
-    """Overrides sdist to make sure that our setup.py is generated."""
-    pass
+	"""Overrides sdist to make sure that our setup.py is generated."""
+	pass
